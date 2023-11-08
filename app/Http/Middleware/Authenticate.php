3464bf266d85769dbd1e9ce16e44fc5031dca3c2
Finn Jesse Dan <jessedan160@gmail.com>
2023-11-08 02:13:27 +0100

    made response better

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,35 +2,42 @@
 
 namespace App\Http\Middleware;
 
-
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Session;
-use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
-class Authenticate 
+class Authenticate
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      */
-    public function handle(Request $request,Closure $next): Response
+    public function handle(Request $request, Closure $next): Response
     {
-        if(!(Session()->has('loginId'))){
+        if (!(Session()->has('loginId'))) {
+
             $user = User::where('id', '=', Session::get('loginId'))->first();
-            if ($user){$user->revoke();
-            return response()->json([
-                'status' => false,
-                'status_code' => 400,
-                'message' => 'No Active session'
-            ], 300); }else{
-                return  response()->json([
+
+            if ($user) {
+
+                $user->revoke();
+                return response()->json([
+                    'status' => false,
+                    'status_code' => 404,
+                    'message' => 'No Active session',
+                ], 404);
+
+            } else {
+
+                return response()->json([
                     'status' => false,
                     'status_code' => 400,
-                    'message' => 'Unauthenticated'
-                ], 300);
+                    'message' => 'Unauthenticated',
+                ], 400);
+
             }
         }
-       return $next($request);
+        return $next($request);
     }
 }
